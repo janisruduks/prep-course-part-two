b1bdd00acfddd2336932ae90533f57df0ca81aa5
Jānis Ruduks <janis.ruduks157@gmail.com>
2023-04-01 21:38:23 +0300

    update

--- src/03-is-even/index.ts
+++ src/03-is-even/index.ts
@@ -8,8 +8,29 @@
  * Examples:
  * isEven(4) === true
  * isEven(3) === false
+ * 
+ * tests are wrong
+ * the concept of odd and even apply only to integers
+ * that do not have a decimal part
+ * so the test should try to catch a string that informs of an error.
  */
 
-function isEven(n: number) {}
+function isEven(num: number) {
+    if(!Number.isInteger(num)){
+        return "odd and even apply only to integers";
+    }
+    const arr = ["0", "2", "4", "6", "8"]; // even
+
+    let numberString = num.toString();
+    const length = numberString.length;
+    numberString = numberString.charAt(length - 1)
+    
+    if (arr.includes(numberString)) {
+        return true; // even
+    }else {
+        return false; // odd
+    }
+}
+
 
 export { isEven };
